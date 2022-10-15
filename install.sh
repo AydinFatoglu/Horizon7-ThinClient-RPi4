1a85f7a84d1fef2fb11382391226f8d2c0771eac
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2022-10-15 16:21:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-#Download Client
+#Download Bundleed Client
 mkdir /home/pi/Downloads/HClients/ 
 cd /home/pi/Downloads/HClients/
 wget https://download3.vmware.com/software/view/viewclients/CART21FQ2/VMware-Horizon-Client-2006-8.0.0-16522670.x86.bundle -p /home/pi/Downloads/HClients/
 cd /home/pi/Downloads/HClients/
 sudo chmod +x ./VMware-Horizon-Client-2006-8.0.0-16522670.x86.bundle
+
+#Download Tarball Client
+cd /home/pi/Downloads/HClients/
+wget https://download3.vmware.com/software/view/viewclients/CART21FQ2/vmware-view-client-linux-2006-8.0.0-16522670.tar.gz
+
+
 
 #Download Install LXDE
 sudo apt install -y lubuntu-desktop
