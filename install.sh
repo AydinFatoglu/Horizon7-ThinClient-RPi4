5355c17df258b2d5862d7b758ed9df0afe62b760
tobleu <tleutritz@aol.de>
2023-03-22 10:42:39 +0100

    Update install.sh to latest downloadable version
    working on bullseye (latest official 32 bit Raspberry pi OS)

--- install.sh
+++ install.sh
@@ -4,21 +4,21 @@ sudo apt-get install raspberrypi-ui-mods -y
 
 
 cd /home/pi/Downloads
-wget https://download3.vmware.com/software/CART23FQ2_LIN_2206_TARBALL/VMware-Horizon-Client-Linux-2206-8.6.0-20094634.tar.gz -O /home/pi/Downloads/VMware-Horizon-Client-Linux-2106.1-8.3.1-18435609.tar.gz
-tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634.tar.gz -C /home/pi/Downloads/
-tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf.tar.gz -C /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/
-tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-PCoIP-2206-8.6.0-20094634.armhf.tar.gz -C /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/
-tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf.tar.gz -C /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/
+wget https://download3.vmware.com/software/CART23FQ4_LIN_2212.1_TARBALL/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348.tar.gz
+tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348.tar.gz -C /home/pi/Downloads/
+tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-Client-2212.1-8.8.1-21219348.armhf.tar.gz -C /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/
+tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-PCoIP-2212.1-8.8.1-21219348.armhf.tar.gz -C /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/
+tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-USB-2212.1-8.8.1-21219348.armhf.tar.gz -C /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/
 
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/bin/ /usr/bin/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/lib/ /usr/lib/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/doc/ /usr/share/doc/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/share/locale/ /usr/share/locale/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-PCoIP-2206-8.6.0-20094634.armhf/lib/ /usr/lib/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-PCoIP-2206-8.6.0-20094634.armhf/lib/vmware/view/client/ /usr/bin
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf/bin/ /usr/bin/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf/lib/ /usr/lib/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf/init.d/ /etc/init.d/
+sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-Client-2212.1-8.8.1-21219348.armhf/bin/ /usr/bin/
+sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-Client-2212.1-8.8.1-21219348.armhf/lib/ /usr/lib/
+sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-Client-2212.1-8.8.1-21219348.armhf/doc/ /usr/share/doc/
+sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-Client-2212.1-8.8.1-21219348.armhf/share/locale/ /usr/share/locale/
+sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-PCoIP-2212.1-8.8.1-21219348.armhf/lib/ /usr/lib/
+sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-PCoIP-2212.1-8.8.1-21219348.armhf/lib/vmware/view/client/ /usr/bin
+sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-USB-2212.1-8.8.1-21219348.armhf/bin/ /usr/bin/
+sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-USB-2212.1-8.8.1-21219348.armhf/lib/ /usr/lib/
+sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-USB-2212.1-8.8.1-21219348.armhf/init.d/ /etc/init.d/
 
 sudo ln -s /usr/bin/vmware-usbarbitrator /usr/lib/vmware/view/usb/vmware-usbarbitrator
 sudo ln -s /usr/bin/vmware-view-usbdloader /usr/lib/vmware/view/usb/vmware-view-usbdloader
@@ -27,7 +27,7 @@ sudo ln -s /etc/init.d/vmware-USBArbitrator /etc/rc6.d/K08vmware-USBArbitrator
 
 sudo sed -i '$i/etc/init.d/vmware-USBArbitrator start' /etc/rc.local
 
-cp /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/share/icons/vmware-view.png /home/pi/Pictures/
+cp /home/pi/Downloads/VMware-Horizon-Client-Linux-2212.1-8.8.1-21219348/armhf/VMware-Horizon-Client-2212.1-8.8.1-21219348.armhf/share/icons/vmware-view.png /home/pi/Pictures/
 
 sudo apt install libgtkmm-3.0-1v5 -y
 sudo apt -y install libudev0
@@ -43,14 +43,3 @@ Categories=Office;
 Terminal=false
 Type=Application
 EOL
-
-
-
-
-
-
-
-
-
-
-
