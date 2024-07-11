085f93c45c6f68ba36c98d60018a014b5b11d97e
AydinFatoglu <86136084+AydinFatoglu@users.noreply.github.com>
2024-07-11 09:55:21 +0300

    Update openbox_ver.sh

--- openbox_ver.sh
+++ openbox_ver.sh
@@ -1,33 +1,12 @@
 #!/bin/bash
 
-# Check if user pi exists, if not, create the user
-if id "pi" &>/dev/null; then
-    echo "User pi exists."
-else
-    echo "User pi does not exist. Creating user pi."
-    sudo useradd -m pi
-    sudo passwd pi
-    sudo usermod -aG sudo pi
-fi
-
-
-# Prompt the user for input
-read -p "Enter the server URL (with https://): " serverURL
-read -p "Enter the domain (NETBIOS) name: " domainName
-read -p "Enter the user name: " userName
-read -s -p "Enter the password: " password
-echo
-read -p "Enter the application name: " applicationName
-
-
 sudo apt-get update -y && 
 sudo apt-get upgrade -y && 
 sudo apt-get install lightdm openbox -y && 
 sudo raspi-config nonint do_boot_behaviour B4 && 
 sudo raspi-config nonint do_boot_wait 0 &&
 sudo raspi-config nonint do_memory_split 256 &&
-sudo apt-get install terminator -y &&
-sudo apt install plymouth plymouth-themes -y && 
+sudo apt-get install terminator -y && 
 
 sudo systemctl disable hciuart.service && 
 sudo systemctl disable bluetooth.service &&
@@ -35,31 +14,30 @@ sudo systemctl mask apt-daily-upgrade &&
 sudo systemctl mask apt-daily && 
 sudo systemctl disable apt-daily-upgrade.timer && 
 sudo systemctl disable apt-daily.timer &&
-
 sudo apt remove --purge avahi-daemon -y && 
 sudo apt remove --purge bluez -y && 
 sudo apt autoremove --purge -y && 
 
-sudo mkdir /home/pi/Downloads
-sudo chown -R pi:pi /home/pi/Downloads
-chmod 755 /home/pi/Downloads
-cd /home/pi/Downloads
+sudo mkdir /home/$USER/Downloads
+sudo chown -R $USER:$USER /home/$USER/Downloads
+chmod 755 /home/$USER/Downloads
+cd /home/$USER/Downloads
 
-wget https://download3.vmware.com/software/CART23FQ2_LIN_2206_TARBALL/VMware-Horizon-Client-Linux-2206-8.6.0-20094634.tar.gz -O /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634.tar.gz
-tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634.tar.gz -C /home/pi/Downloads/
-tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf.tar.gz -C /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/
-tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-PCoIP-2206-8.6.0-20094634.armhf.tar.gz -C /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/
-tar -xzf /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf.tar.gz -C /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/
+wget https://download3.vmware.com/software/CART23FQ2_LIN_2206_TARBALL/VMware-Horizon-Client-Linux-2206-8.6.0-20094634.tar.gz -O /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634.tar.gz
+tar -xzf /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634.tar.gz -C /home/$USER/Downloads/
+tar -xzf /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf.tar.gz -C /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/
+tar -xzf /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-PCoIP-2206-8.6.0-20094634.armhf.tar.gz -C /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/
+tar -xzf /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf.tar.gz -C /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/
 
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/bin/ /usr/bin/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/lib/ /usr/lib/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/doc/ /usr/share/doc/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/share/locale/ /usr/share/locale/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-PCoIP-2206-8.6.0-20094634.armhf/lib/ /usr/lib/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-PCoIP-2206-8.6.0-20094634.armhf/lib/vmware/view/client/ /usr/bin
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf/bin/ /usr/bin/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf/lib/ /usr/lib/
-sudo cp -aRT /home/pi/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf/init.d/ /etc/init.d/
+sudo cp -aRT /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/bin/ /usr/bin/
+sudo cp -aRT /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/lib/ /usr/lib/
+sudo cp -aRT /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/doc/ /usr/share/doc/
+sudo cp -aRT /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-Client-2206-8.6.0-20094634.armhf/share/locale/ /usr/share/locale/
+sudo cp -aRT /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-PCoIP-2206-8.6.0-20094634.armhf/lib/ /usr/lib/
+sudo cp -aRT /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-PCoIP-2206-8.6.0-20094634.armhf/lib/vmware/view/client/ /usr/bin
+sudo cp -aRT /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf/bin/ /usr/bin/
+sudo cp -aRT /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf/lib/ /usr/lib/
+sudo cp -aRT /home/$USER/Downloads/VMware-Horizon-Client-Linux-2206-8.6.0-20094634/armhf/VMware-Horizon-USB-2206-8.6.0-20094634.armhf/init.d/ /etc/init.d/
 
 sudo ln -s /usr/bin/vmware-usbarbitrator /usr/lib/vmware/view/usb/vmware-usbarbitrator
 sudo ln -s /usr/bin/vmware-view-usbdloader /usr/lib/vmware/view/usb/vmware-view-usbdloader
@@ -74,6 +52,17 @@ sudo apt install libxss1 -y
 
 mkdir -p ~/.config/openbox && 
 
+# Prompt the user for input
+read -p "Enter the server URL: " server
+read -p "Enter the username: " user
+read -sp "Enter the password: " ebipass
+echo
+read -p "Enter the domain name: " domain
+read -p "Enter the application name: " app
+
+
+
+
 # Create the autostart file with the provided values
 cat << EOF > ~/.config/openbox/autostart
 # Disable DPMS and prevent screen blanking
@@ -86,16 +75,13 @@ xsetroot -solid "#7393B3" &
 
 # Loop to keep vmware-view running
 while true; do
-    vmware-view  --serverURL $serverURL --useExisting --userName $userName --password $password --domainName $domainName --nonInteractive --applicationName "$applicationName" &
+    vmware-view  --serverURL $server --useExisting --userName $user --password $ebipass --domainName $domain --nonInteractive --applicationName "$app" &
     wait \$!
 done &
 EOF
 
 echo "File created at ~/.config/openbox/autostart"
 
-sudo plymouth-set-default-theme -R bgrt && 
-sudo update-initramfs -u && 
-sudo sed -i 's/$/ quiet splash/' /boot/cmdline.txt && 
 
 sudo reboot
 
